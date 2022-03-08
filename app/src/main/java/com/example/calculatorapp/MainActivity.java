458582829f610de0ae1bfe7f6bd5392d320f1ac6
Gal Shamir <galshamir8@gmail.com>
2022-03-08 19:35:11 +0200

    start building main implementation

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private static final String TAG = "main activity";
     //first layout
     private EditText main_EDT_first_number;
     private EditText main_EDT_second_number;
@@ -28,12 +34,62 @@ public class MainActivity extends AppCompatActivity {
     private EditText main_EDT_name;
     private EditText main_EDT_id;
 
+    private HashMap<String, Integer> calcMap = new HashMap<>();
+    private String[] funcOpt = {"x", "%", "+", "-"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setViews();
+        setClickListeners();
+    }
+
+    private void setClickListeners() {
+        main_EDT_first_number.setOnClickListener(e -> handleNumberInput(1));
+        main_EDT_second_number.setOnClickListener(e -> handleNumberInput(2));
+        main_CMB_func_opt.setOnClickListener(e -> handleFuncInput());
+        main_BTN_calc.setOnClickListener(e -> handleCalcBtn());
+    }
+
+    private void handleCalcBtn() {
+    }
+
+    private void handleFuncInput() {
+        try{
+            funcStr = main_CMB_func_opt.get
+        }catch (Exception e){
+            Log.e(TAG ,"failed to parse string to int", e.getCause());
+            return;
+        }
+    }
+
+    private void handleNumberInput(int index) {
+        String numStr = "";
+        int value = 0 ;
+        switch (index){
+            case 1:
+                numStr = main_EDT_first_number.getText().toString();
+                try {
+                    value = Integer.parseInt(numStr);
+                }catch(Exception e){
+                    Log.e(TAG ,"failed to parse string to int", e.getCause());
+                    return;
+                }
+                calcMap.put("first", value);
+                break;
+            case 2:
+                numStr = main_EDT_second_number.getText().toString();
+                try {
+                    value = Integer.parseInt(numStr);
+                }catch(Exception e){
+                    Log.e(TAG ,"failed to parse string to int", e.getCause());
+                    return;
+                }
+                calcMap.put("second", value);
+                break;
+        }
     }
 
     private void setViews() {
